850af2b3f000e3fb2db4dbdcb0cefbe4a70758c8
rosenholmjohanna <rosenholm.johanna88@gmail.com>
2024-09-23 16:33:00 +0200

    #116 created projectMembersList component showing members with avatar. Created data folder with mockdata for project members

---
+++ data/MockMembers.js
@@ -0,0 +1,5 @@
+export const mockMembers = [
+    {name: "Bob", id: "1", profileImage: "https://images.pexels.com/photos/3823495/pexels-photo-3823495.jpeg?auto=compress&cs=tinysrgb&w=800"}, 
+    { name: 'Johan', id: "2", profileImage: "https://images.pexels.com/photos/3046636/pexels-photo-3046636.jpeg?auto=compress&cs=tinysrgb&w=800" },
+    { name: 'Jenny', id: "3", profileImage: "https://images.pexels.com/photos/3473525/pexels-photo-3473525.jpeg?auto=compress&cs=tinysrgb&w=800" },
+];
