975f832d81089898c0b7533c88049ac533c0e949
ellinor Jansson lande <ellinor_jansson@hotmail.com>
2024-10-01 15:59:04 +0200

    avatar section in workspace is moved and added moduel css to all components

--- src/avatar/MembersListAvatar.jsx
+++ src/avatar/MembersListAvatar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Avatar } from "@niklaspelli/fwk4-23-components";
 import { newMockUsers } from "../../data/MockMembers"; // Se till att sökvägen är korrekt
-import './MembersListAvatar.css'; 
+import styles from "./MembersListAvatar.module.css";
 
 const MembersListAvatar = () => {
   return (
     <>
     <h4>Members in this project:</h4>
-      <ul className="avatar-list">
+      <ul className={styles.avatarlist}>
         {newMockUsers.map((user, id) => (
           <li key={id}>
             <Avatar src={user.profileImage} />
