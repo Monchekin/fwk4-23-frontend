23eb8417aa8e6ac2a8ee1c76c4f1891e6a8bd350
Lina.barthelson@gmail.com <lina.barthelson@gmail.com>
2024-10-01 12:02:02 +0200

    Two links in footer that open in new tabs

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,11 +1,12 @@
 import "./Footer.modules.css";
 
 const Footer = () => {
+  const subSubTitle = "Information:";
   return (
     <footer className="footer">
-      <a href="">Länk</a>
-
-      <a href="">Länk</a>
+      <h3 className="subsubtitle">{subSubTitle}</h3>      
+      <a href="https://github.com/Niklaspelli/fwk4-23-components"  target="_blank">- about Components</a>
+      <a href="https://github.com/Monchekin/fwk4-23-frontend"  target="_blank">- about Frontend</a>
     </footer>
   );
 };
